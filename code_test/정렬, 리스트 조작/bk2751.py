72225a1a3453106db2dc7cbc0b7a90e52952df88
milcho0604 <milcho0604@gmail.com>
2025-08-10 20:30:23 +0900

    2751 int형 변환 및 시간 초과 해결

--- code_test/정렬, 리스트 조작/bk2751.py
+++ code_test/정렬, 리스트 조작/bk2751.py
@@ -1,14 +1,20 @@
+import sys
+
+input = sys.stdin.readline
 n = int(input())
 
 result = []
 
 for _ in range(n):
-    result.append(input())
+    result.append(int(input()))
 
 result.sort()
 
-for i in range(len(result)):
-    print(result[i])
+for i in result:
+    print(i)
+
+# for i in range(len(result)):
+    # print(result[i])
     
     # 만약 한 줄로 1 2 3 4 5와 같이 출력하고 싶다면 아래 end 추가
     # print(result[i], end=' ')
