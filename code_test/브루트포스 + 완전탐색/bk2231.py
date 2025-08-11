db32b092456fa5ef4f7616c2f903c5037ee8b025
milcho0604 <milcho0604@gmail.com>
2025-08-11 13:36:28 +0900

    백준 1436 파이썬

--- code_test/브루트포스 + 완전탐색/bk2231.py
+++ code_test/브루트포스 + 완전탐색/bk2231.py
@@ -10,11 +10,15 @@ ranges = n - (9 * int(len(str(n))))
 for i in range(ranges, n):
     num = i
     sum = 0
+    print('첫 번째: ', num)
 
     # /10 방식으로 각 자릿수 합 구하기
     while num > 0:
+        print('i: ', i)
         sum += num % 10  # 마지막 자리 더하기
+        print('두 번째: ',sum)
         num //= 10  # 마지막 자리 제거
+        print('세 번째: ',num)
 
     total = i + sum  # 분해합
 
